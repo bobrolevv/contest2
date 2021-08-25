77d00a2b56eb5db31a38a38a06f9f5fc57ff38ff
Art <bobrolevv@yandex.ru>
2021-08-25 17:25:49 +0700

    132

--- 123.py
+++ 123.py
@@ -1,18 +1,13 @@
-# работа из дома
 d = int(input())
 res = d
 stroka = ""
 while res != 1:
     if res % 2:
         stroka = (stroka + "1")
-        #print(f'=={res % 2}=={stroka}')
     else:
         stroka = (stroka + "0")
-        #print(f'=={res % 2}=={stroka}')
-
-    if res == 2:
+    if res <= 3:
         stroka = (stroka + "1")
-		break
+
     res = res // 2
-    #print(f'=={res % 2}=={stroka}')
 print(stroka[::-1])
